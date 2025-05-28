750d76842c217cacdd701d513d0d5b42e61e9f68
nanocode <101373394+nanocode00@users.noreply.github.com>
2025-05-29 06:43:44 +0900

    delete

--- verilog/ImmediateExtender.v
+++
@@ -1,9 +0,0 @@
-module ImmediateExtender(
-    input [15:0] ImmIn,
-    input ZeroExtend,
-    output [31:0] ImmOut
-);
-
-    assign ImmOut = ZeroExtend ? {16'h0000, ImmIn} : {{16{ImmIn[15]}}, ImmIn};
-
-endmodule
